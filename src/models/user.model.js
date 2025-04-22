f38bd42cf281023740dc8904e7259ea9787f953d
TonyLe <149919663+TrungKien0206@users.noreply.github.com>
2025-04-22 23:24:37 +0700

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,29 +3,25 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import routes from "./src/routes/index.js";
-import connectDB from "./src/config/db.js"; // 👈 thêm dòng này
-import User from "./src/models/user.model.js"; // 👈 dùng model để đọc DB
+import connectDB from "./src/config/db.js"; 
+import User from "./src/models/user.model.js"; 
 
 const app = express();
 dotenv.config();
-connectDB(); // 👈 gọi kết nối MongoDB
+connectDB(); 
 
-// __dirname setup cho ESM
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// View engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "src", "views"));
 
-// Static
 app.use("/public", express.static(path.join(__dirname, "src", "public")));
 app.use(express.json());
 
-// Routes
 app.use("/api", routes);
 
-// Trang chủ
 app.get("/", async (req, res) => {
   try {
     const users = await User.find();
@@ -44,7 +40,6 @@ app.get("/", async (req, res) => {
   }
 });
 
-// Server listen
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`🚀 Server running at http://localhost:${port}`);
